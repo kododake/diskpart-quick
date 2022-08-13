3e17ac61d768d970c9196b15b7965cf1631367ad
kododake <80098041+kododake@users.noreply.github.com>
2022-08-13 20:30:32 +0900

    Update diskpart-quick.cmd

--- diskpart-quick.cmd
+++ diskpart-quick.cmd
@@ -13,7 +13,7 @@ set /p selectdisk=select disk(num):
 echo; > diskpart-quick.tmp
 echo select disk %selectdisk% >> diskpart-quick.tmp
 echo; >>diskpart-quick.tmp
-set /p parnum=!final![Number of partitions greater than expected](num):
+set /p parnum=!final![Please enter more than the expected number of partitions](num):
 FOR /L %%i IN (1,1,%parnum%) DO (
 echo select partition %%i >> diskpart-quick.tmp
 echo; >>diskpart-quick.tmp
@@ -23,12 +23,13 @@ echo; >>diskpart-quick.tmp
 echo ====LOG===========
 diskpart /s diskpart-quick.tmp
 echo;
-echo; > diskpart-quick.tmp
-echo list disk >> diskpart-quick.tmp
-diskpart /s diskpart-quick.tmp
+echo; > diskpart-quick2.tmp
+echo list disk >> diskpart-quick2.tmp
+diskpart /s diskpart-quick2.tmp
 echo;
 echo [-----COMPLETE-----]
 echo;
 echo Press Enter to delete the temporary file.
 pause
 del diskpart-quick.tmp
+del diskpart-quick2.tmp
